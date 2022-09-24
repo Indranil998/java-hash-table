b168e9054bd37df23b7375f193f2604a6e0bd188
Indranil998 <indranilm1998@gmail.com>
2022-09-24 21:11:03 +0530

    ADDED: all project file

---
+++ src/com/hashtable/Entry.java
@@ -0,0 +1,17 @@
+package com.hashtable;
+
+public class Entry<Key, Value> {
+	public Key key;
+	public Value value;
+	
+	/**
+	 * Create class and wrap key value pair data into that
+	 * 
+	 * @param key
+	 * @param value
+	 */
+	Entry(Key key, Value value) {
+		this.key = key;
+		this.value = value;
+	}
+}
